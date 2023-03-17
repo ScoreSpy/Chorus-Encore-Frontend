88367882481bc87561aee72e17d71a91daa9bc9d
Ahriana <ahrianadev@gmail.com>
2023-03-17 16:01:07 +0000

    move search to home

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -22,28 +22,6 @@
       </v-row>
     </v-system-bar>
 
-    <v-app-bar app color="#2c3e50" name="app-bar" class="justify-center">
-      <v-row class="justify-center">
-
-        <v-col class="d-flex" xs="0" sm="0" md="3" lg="3" xl="3"></v-col>
-
-        <v-col class="d-flex align-center" cols="12" md="6">
-          <v-text-field
-            color="primary"
-            density="compact"
-            label="What do you feel like playing today?"
-            append-inner-icon="mdi-magnify"
-            single-line
-            hide-details
-          ></v-text-field>
-        </v-col>
-
-        <v-col class="d-none d-md-block" xs="0" sm="0" md="3" lg="3" xl="3">
-          <v-btn variant="text" color="#DA9E0E">Advanced Search</v-btn>
-        </v-col>
-      </v-row>
-    </v-app-bar>
-
     <DefaultView />
   </v-app>
 </template>
