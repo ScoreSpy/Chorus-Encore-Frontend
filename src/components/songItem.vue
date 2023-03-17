8d391b2aa0a80b7b2fd6d2143d711aad24ad8525
Ahriana <ahrianadev@gmail.com>
2023-03-17 15:58:31 +0000

    song item stub

---
+++ src/components/songItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    owo
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+})
+</script>
